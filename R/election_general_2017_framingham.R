13f1d2ce6cbf7be91f8b2f405cb2014e6b4f3b81
smach <smachlis@gmail.com>
2017-11-18 22:37:56 -0500

    Finished general election 2017

--- R/election_general_2017_framingham.R
+++ R/election_general_2017_framingham.R
@@ -1,7 +1,8 @@
+source("R/config.R")
 source("R/election_helpers.R")
 
 working_directory <- getwd()
-datafile <- paste0(working_directory, "/data-raw/elections/election_general_2017_framingham.xlsx")
+datafile <- paste0(working_directory, "/data-raw/elections/election_general_2017_framingham_raw.xlsx")
 
 mayor <- readxl::read_xlsx(datafile, range = "A6:S9", col_names = election_wide_column_names)
 
@@ -52,4 +53,115 @@ turnout_tidy <- dcast(turnout_tidy, Precinct ~ Candidate) %>%
     District = get_district(Precinct)
   )
 
+turnout_by_precinct <- turnout_tidy %>%
+  select(Precinct, `Total Voter Turnout`, `Total Registered Voters`) %>%
+  mutate(
+    Turnout = round(`Total Voter Turnout` / `Total Registered Voters`, 3) * 100,
+    Election = "General",
+    Year = 2017
+  ) %>%
+  janitor::clean_names()
 
+
+rio::export(turnout_by_precinct, file = "data/elections/turnout_election_general_2017_framingham.csv")
+
+turnout_by_district <- turnout_tidy %>%
+ janitor::clean_names() %>%
+  select(district, total_voter_turnout, total_registered_voters) %>%
+  group_by(district) %>%
+  summarize(
+    total_voter_turnout = sum(total_voter_turnout),
+    total_registered_voters = sum(total_registered_voters),
+    Turnout = round(total_voter_turnout / total_registered_voters, 3) * 100,
+    Election = "General",
+    Year = 2017
+  )
+
+rio::export(turnout_by_district, file = "data/elections/turnout_bydistrict_election_general_2017_framingham.csv")
+
+election_general_2017_framingham_tidy <- bind_rows(mayor_tidy, atlarge_tidy, district_council_tidy, school_committee_tidy)
+
+rio::export(election_general_2017_framingham_tidy, file = "data/elections/election_general_2017_framingham_tidy.csv")
+
+mayor_summary <- mayor_tidy %>%
+  select(Precinct, Candidate, Votes) %>%
+  dcast(Precinct ~ Candidate, value.var = "Votes") %>%
+  janitor::adorn_totals() %>%
+  mutate(
+    TotalVotesCast = `John A. Stefanini` + `Yvonne M. Spicer` + `Write-Ins`,
+    SpicerPercent = round((`Yvonne M. Spicer` / TotalVotesCast) * 100, 1),
+    StefaniniPercent = round((`John A. Stefanini` / TotalVotesCast) * 100, 1)
+  )
+  
+
+mayor_summary_by_district <- mayor_summary %>%
+  mutate(
+    District = get_district(Precinct)
+   ) %>%
+  group_by(District) %>%
+  summarize(
+    Spicer = sum(`Yvonne M. Spicer`),
+    Stefanini = sum(`John A. Stefanini`),
+    TotalVotesCast = sum(TotalVotesCast),
+    SpicerPct = round((Spicer / TotalVotesCast) * 100, 1),
+    StefaniniPct = round((Stefanini / TotalVotesCast) * 100, 1)
+  )
+mayor_summary_by_district[10,1] <- "Total"
+
+rio::export(mayor_summary, file = "data/elections/mayor_election_general_2017_framingham.csv")
+rio::export(mayor_summary_by_district, file = "data/elections/mayor_bydistrict_election_general_2017_framingham.csv")
+
+atlarge_summary <- atlarge_tidy %>%
+  select(Precinct, Candidate, Votes) %>%
+  dcast(Precinct ~ Candidate, value.var = "Votes") %>%
+  janitor::adorn_totals() %>%
+  select(Precinct, TullyStoll = `Cheryl Tully Stoll`, King = `George P. King, Jr.`,
+         Long = `Christine A. Long`, Maia = `Pablo Maia`, WriteIns = `Write-Ins`) %>%
+  mutate(
+    TotalVotesCast = TullyStoll + King + Long + Maia + WriteIns,
+    TullyStollPercent = round((TullyStoll / TotalVotesCast) * 100, 1),
+    KingPercent = round((King / TotalVotesCast) * 100, 1),
+    LongPercent = round((Long / TotalVotesCast) * 100, 1),
+    MaiaPercent = round((Maia / TotalVotesCast) * 100, 1)
+  )
+
+atlarge_summary_by_district <- atlarge_summary %>%
+  mutate(
+    District = get_district(Precinct)
+  ) %>%
+  group_by(District) %>%
+  summarize(
+    TullyStoll = sum(TullyStoll),
+    King = sum(King),
+    Long = sum(Long),
+    Maia = sum(Maia),
+    TotalVotesCast = sum(TotalVotesCast)
+  )
+atlarge_summary_by_district[10,1] <- "Total" 
+
+rio::export(atlarge_summary, file = "data/elections/atlarge_election_general_2017_framingham.csv")
+rio::export(atlarge_summary_by_district, file = "data/elections/atlarge_bydistrict_election_general_2017_framingham.csv")
+
+district_council_by_district_tidy <- district_council_tidy %>%
+  mutate(
+    District = get_district(Precinct)
+  ) %>%
+  group_by(Race, Candidate) %>%
+    summarize(
+      Votes = sum(Votes)
+    ) %>%
+  arrange(Race, desc(Votes))
+
+rio::export(district_council_by_district_tidy, file = "data/elections/district_council_bydistrict_tidy_election_general_2017_framingham.csv")
+
+school_committee_by_district_tidy <- school_committee_tidy %>%
+  mutate(
+    District = get_district(Precinct)
+  ) %>%
+  group_by(Race, Candidate) %>%
+  summarize(
+    Votes = sum(Votes)
+  ) %>%
+  arrange(Race, desc(Votes))
+
+rio::export(school_committee_by_district_tidy, file = "data/elections/school_committee_bydistrict_tidy_election_general_2017_framingham.csv")
